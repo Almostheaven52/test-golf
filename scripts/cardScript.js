d41a4809b7e62286d7af5183f7d9642bd3164eb6
Almostheaven52 <97544927+Almostheaven52@users.noreply.github.com>
2022-10-24 18:28:18 -0800

    Delete cardScript.js

--- scripts/cardScript.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(".cardClose").click(function () {
-    $(this).parent().hide();
-  });  
-
-  $(".cardOpen").click(function () {
-    $(this).parent().children().show();
-  });
-});
